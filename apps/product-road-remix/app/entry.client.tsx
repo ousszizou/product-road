3a274912df390661f8bd8f532f6b1c93875d0990
ousszizou <odjaidri@gmail.com>
2022-09-04 16:16:15 +0100

    feat: add Chakra UI for styling

--- apps/product-road-remix/app/entry.client.tsx
+++ apps/product-road-remix/app/entry.client.tsx
@@ -1,4 +1,32 @@
-import { RemixBrowser } from "@remix-run/react";
-import { hydrateRoot } from "react-dom/client";
+import React, { useState } from "react";
 
-hydrateRoot(document, <RemixBrowser />);
+import { CacheProvider } from "@emotion/react";
+import { ClientStyleContext } from "./context";
+import { RemixBrowser } from "@remix-run/react";
+import createEmotionCache from "./createEmotionCache";
+import { hydrate } from "react-dom";
+
+interface ClientCacheProviderProps {
+  children: React.ReactNode;
+}
+
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = useState(createEmotionCache());
+
+  function reset() {
+    setCache(createEmotionCache());
+  }
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  );
+}
+
+hydrate(
+  <ClientCacheProvider>
+    <RemixBrowser />
+  </ClientCacheProvider>,
+  document
+);
--- apps/product-road-remix/app/entry.server.tsx
+++ apps/product-road-remix/app/entry.server.tsx
@@ -1,5 +1,9 @@
-import type { EntryContext } from "@remix-run/node";
+import { CacheProvider } from "@emotion/react";
+import type { EntryContext } from "@remix-run/node"; // Depends on the runtime you choose
 import { RemixServer } from "@remix-run/react";
+import { ServerStyleContext } from "./context";
+import createEmotionCache from "./createEmotionCache";
+import createEmotionServer from "@emotion/server/create-instance";
 import { renderToString } from "react-dom/server";
 
 export default function handleRequest(
@@ -8,13 +12,30 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  const html = renderToString(
+    <ServerStyleContext.Provider value={null}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>
+  );
+
+  const chunks = extractCriticalToChunks(html);
+
   const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
+    <ServerStyleContext.Provider value={chunks.styles}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>
   );
 
   responseHeaders.set("Content-Type", "text/html");
 
-  return new Response("<!DOCTYPE html>" + markup, {
+  return new Response(`<!DOCTYPE html>${markup}`, {
     status: responseStatusCode,
     headers: responseHeaders,
   });
